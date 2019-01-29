536fd74597c01db2eb543e99179e45a81cfc52bb
saud maruli <uthrul@gmail.com>
2019-01-29 08:19:58 +0700

    creating web applications with go

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content Type", "text/html")
+		tmpl, err := template.New("test").Parse(doc)
+		if err == nil {
+			tmpl.Execute(w, nil)
+		}
+	})
+
+	http.ListenAndServe(":8000", nil)
+}
+
+const doc = `
+<!DOCTYPE html>
+<html>
+  <head><title>Example Title</title></head>
+  <body>
+    <h1>Hello Templates</h1>
+  </body>
+</html>
+`
