17ef2d85bed1c1c84ebabe08e3f7c04bc06327e1
saud maruli <uthrul@gmail.com>
2019-01-29 14:52:03 +0700

    The View Layer

---
+++ README.md
@@ -0,0 +1,16 @@
+# web go
+Writing a Web Application in Go.
+=======================================
+
+0. package
+```
+package main
+
+func main() {
+	
+}
+```
+
+1. run 
+- cd GoWebApp
+- go run main.go
--- main.go
+++ main.go
@@ -1,52 +1,135 @@
 package main
 
 import (
+	"bufio"
 	"net/http"
+	"os"
+	"strings"
 	"text/template"
+
+	"fmt"
+
+	"log"
+
+	"github.com/erdalkiran/pluralsight-go-creating-web-apps/constants"
+	"github.com/erdalkiran/pluralsight-go-creating-web-apps/viewmodels"
+)
+
+const (
+	TEMPLATES_FOLDER = "templates"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Add("Content Type", "text/html")
-		templates := template.New("template")
-		templates.New("test").Parse(doc)
-		templates.New("header").Parse(header)
-		templates.New("footer").Parse(footer)
-		context := Context{
-			[5]string{"Banana", "Orange", "Apple", "Tomato", "Potato"},
-			"the title",
+	templates, err := populateTemplates()
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	http.HandleFunc(constants.Paths.ROOT(), func(resp http.ResponseWriter, req *http.Request) {
+		requestedFile := req.URL.Path[1:]
+
+		template := templates.Lookup(requestedFile + ".html")
+		if template == nil {
+			fmt.Println("unable to find template")
+			resp.WriteHeader(404)
+			return
 		}
-		templates.Lookup("test").Execute(w, context)
+
+		err := template.Execute(resp, getContext(requestedFile))
+		if err != nil {
+			log.Fatal(err)
+		}
 	})
+
+	http.HandleFunc(constants.Paths.CSS(), serveResource)
+	http.HandleFunc(constants.Paths.IMAGE(), serveResource)
 
 	http.ListenAndServe(":8000", nil)
 }
 
-const doc = `
-{{template "header" .Title}}
-  <body>
-    <h1>List of Fruit</h1>
-    <ul>
-      {{range .Fruit}}
-      	<li>{{.}}</li>
-      {{end}}
-    </ul>
-  </body>
-{{template "footer"}}
-`
+func getContext(requestedFile string) interface{} {
+	var context interface{}
+	if requestedFile == "home" {
+		context = viewmodels.NewHome()
+	} else if requestedFile == "categories" {
+		context = viewmodels.NewCategories()
+	} else if requestedFile == "products" {
+		context = viewmodels.NewProducts()
+	} else if requestedFile == "product" {
+		context = viewmodels.NewProduct()
+	}
+	return context
+}
 
-const header = `
-<!DOCTYPE html>
-<html>
-  <head><title>{{.}}</title></head>
-`
+func serveResource(resp http.ResponseWriter, req *http.Request) {
+	resourcePath := "public" + req.URL.Path
+	file, err := os.Open(resourcePath)
+	defer file.Close()
 
-const footer = `
-</html>
-	<p>copywrite 2019</p>
-`
+	if err != nil {
+		resp.WriteHeader(404)
+		return
+	}
 
-type Context struct {
-	Fruit [5]string
-	Title string
+	resp.Header().Add("content-type", getContentType(req.URL.Path))
+	reader := bufio.NewReader(file)
+	reader.WriteTo(resp)
+
+}
+
+func getContentType(path string) string {
+	var contentType string
+
+	if strings.HasSuffix(path, ".css") {
+		contentType = "text/css"
+	} else if strings.HasSuffix(path, ".png") {
+		contentType = "image/png"
+	} else {
+		contentType = "text/plain"
+	}
+
+	return contentType
+}
+
+func populateTemplates() (*template.Template, error) {
+	templatePaths, err := getTemplatePaths(TEMPLATES_FOLDER)
+	if err != nil {
+		return nil, err
+	}
+	result := template.New("mainTemplate")
+	_, err = result.ParseFiles(templatePaths...)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func getTemplatePaths(basePath string) ([]string, error) {
+	templateFolder, err := os.Open(basePath)
+	if err != nil {
+		return nil, err
+	}
+
+	defer templateFolder.Close()
+
+	templatePathsRaw, err := templateFolder.Readdir(-1)
+	if err != nil {
+		return nil, err
+	}
+	templatePaths := new([]string)
+	for _, pathInfo := range templatePathsRaw {
+		if pathInfo.IsDir() {
+			dirTemplatePaths, err := getTemplatePaths(basePath + "/" + pathInfo.Name())
+			if err != nil {
+				return nil, err
+			}
+			*templatePaths = append(*templatePaths, dirTemplatePaths...)
+			continue
+		}
+
+		*templatePaths = append(*templatePaths, basePath+"/"+pathInfo.Name())
+	}
+
+	return *templatePaths, nil
 }
